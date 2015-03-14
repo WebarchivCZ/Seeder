98b5361dd21946a19dcace053cccdc1e6b8110e8
Visgean Skeloru <visgean@gmail.com>
2015-03-14 02:13:02 +0100

    micro refactoring

--- Seeder/Seeder/settings/base.py
+++ Seeder/Seeder/settings/base.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'django_pg'
+    'django_pg',
 )
 
 MIDDLEWARE_CLASSES = (
--- Seeder/core/models.py
+++ Seeder/core/models.py
@@ -20,7 +20,7 @@ class Publisher(BaseModel):
         Publisher of the Source(s), Publisher can have multiple contacts.
     """
     name = models.CharField(_('Name'), max_length=64)
-    contacts = models.ArrayField(of=models.CharField())
+    contacts = models.ArrayField(models.CharField(max_length=64))
 
     class Meta:
         verbose_name = _('Publisher')
