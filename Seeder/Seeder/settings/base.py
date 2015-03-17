0dd87c27a5ff9fdae132521a1d0215b8d2416079
Visgean Skeloru <visgean@gmail.com>
2015-03-17 04:32:37 +0100

    proper static_root for collected static files

--- Seeder/Seeder/settings/base.py
+++ Seeder/Seeder/settings/base.py
@@ -79,4 +79,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static_root')
