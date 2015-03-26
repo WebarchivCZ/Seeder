bc58ff40b98194b2c24b076c234ed3bbd2d065da
Visgean Skeloru <visgean@gmail.com>
2015-03-26 02:11:43 +0100

    utils: merge dict
    see http://stackoverflow.com/questions/38987/how-can-i-merge-two-python-dictionaries-in-a-single-expression

--- Seeder/core/utils.py
+++ Seeder/core/utils.py
@@ -1,1 +1,33 @@
-__author__ = 'visgean'
+from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+class LoginMixin(object):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginMixin, self).dispatch(request, *args, **kwargs)
+
+
+class ProjectPage(TemplateView):
+    """
+    This is used for easy declaration of titles and view_name that are used in
+    menus to set current menu to active state.
+    """
+    title = 'Project'
+    view_name = 'project'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectPage, self).get_context_data(**kwargs)
+        context['title'] = self.title
+        context['view_name'] = self.view_name
+        return context
+
+
+def merge_dicts(x, y):
+    """
+    Given two dicts, merge them into a new dict as a shallow copy.
+    """
+    z = x.copy()
+    z.update(y)
+    return z
