eb68571cbf259f1b2cbff27ea0dc09c38faa2d52
Visgean Skeloru <visgean@gmail.com>
2015-04-02 19:15:23 +0200

    more fields in sources table

--- Seeder/core/tables.py
+++ Seeder/core/tables.py
@@ -5,7 +5,8 @@ import django_tables2 as tables
 class SourceTable(tables.Table):
     class Meta:
         model = models.Source
-        fields = ('name', 'owner', 'created', 'last_changed', 'state')
+        fields = ('name', 'owner', 'created', 'last_changed', 'state',
+                  'publisher', 'conspectus', 'sub_conspectus')
         attrs = {
             'class': 'table table-striped'
         }
