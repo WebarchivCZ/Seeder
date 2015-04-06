2e7f3af01e7b5b41ebb9653dba9c76ad8f79ff7e
Visgean Skeloru <visgean@gmail.com>
2015-04-06 20:57:44 +0200

    Empty choices and more fields in filters... #26

--- Seeder/core/field_filters.py
+++ Seeder/core/field_filters.py
@@ -1,9 +1,35 @@
-import models
+import models as core_models
+import django_filters
 
-from django_filters import FilterSet
+from django.db import models
+from django_filters.filters import ChoiceFilter
+
+EMPTY_CHOICE = ('', '---------')
 
 
-class SourceFilter(FilterSet):
+class SourceFilter(django_filters.FilterSet):
+    filter_overrides = {
+        models.CharField: {
+            'filter_class': django_filters.CharFilter,
+            'extra': lambda f: {
+                'lookup_type': 'icontains',
+            }
+        }
+    }
+    
+    def __init__(self, *args, **kwargs):
+        super(SourceFilter, self).__init__(*args, **kwargs)
+        # add empty choice to all choice fields:
+        choices = filter(
+            lambda f: isinstance(self.filters[f], ChoiceFilter),
+            self.filters)
+
+        for field_name in choices:
+            extended_choices = ((EMPTY_CHOICE,) +
+                                self.filters[field_name].extra['choices'])
+            self.filters[field_name].extra['choices'] = extended_choices
+
     class Meta:
-        model = models.Source
-        fields = ('name', 'publisher')
+        model = core_models.Source
+        fields = ('owner', 'name', 'web_proposal', 'publisher', 'state',
+                  'conspectus', 'sub_conspectus', 'created',  'last_changed')
