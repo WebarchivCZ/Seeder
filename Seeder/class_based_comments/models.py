b53457b0f340797fa9ede5fad18a6f84593859bf
Visgean Skeloru <visgean@gmail.com>
2015-04-22 00:19:18 +0200

    Refactored model from django-contrib-comments
    My model ommits website link and is according to pep8 I have also refactored submit date field so its default is now callable instead of overriding the save method:
    https://github.com/django/django-contrib-comments/blob/master/django_comments/models.py#L86 (ew gross)

---
+++ Seeder/class_based_comments/models.py
@@ -0,0 +1,113 @@
+from django.conf import settings
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
+
+COMMENT_MAX_LENGTH = getattr(settings, 'COMMENT_MAX_LENGTH', 3000)
+
+
+class Comment(models.Model):
+    """
+        A user comment about some object.
+    """
+    # Content-object field
+    content_type = models.ForeignKey(
+        ContentType,
+        verbose_name=_('content type'),
+        related_name="content_type_set_for_%(class)s")
+
+    object_pk = models.TextField(_('object ID'))
+    content_object = generic.GenericForeignKey(ct_field="content_type",
+                                               fk_field="object_pk")
+    # Identity fields:
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        verbose_name=_('user'),
+        blank=True,
+        null=True,
+        related_name="%(class)s_comments")
+    user_name = models.CharField(_("user's name"), max_length=50, blank=True)
+    user_email = models.EmailField(_("user's email address"), blank=True)
+
+    ip_address = models.GenericIPAddressField(
+        verbose_name=_('IP address'),
+        protocol='both',
+        unpack_ipv4=True,
+        blank=True,
+        null=True)
+
+    comment = models.TextField(_('comment'), max_length=COMMENT_MAX_LENGTH)
+
+    # Metadata about the comment
+    submit_date = models.DateTimeField(
+        _('date/time submitted'),
+        default=lambda: timezone.now())
+
+    is_public = models.BooleanField(
+        verbose_name=_('is public'),
+        default=True,
+        help_text=_('Uncheck this box to make the comment effectively '
+                    'disappear from the site.'))
+
+    is_removed = models.BooleanField(
+        verbose_name=_('is removed'),
+        default=False,
+        help_text=_('Check this box if the comment is inappropriate. A "This '
+                    'comment has been removed" message will be displayed '
+                    'instead.'))
+
+    def _get_user_info(self):
+        """
+        Get a dictionary that pulls together information about the poster
+        safely for both authenticated and non-authenticated comments.
+        This dict will have ``name``, ``email``, and ``url`` fields.
+        """
+        if not hasattr(self, "_user_info"):
+            user_info = {
+                "name": self.user_name,
+                "email": self.user_email,
+            }
+            if self.user_id:
+                u = self.user
+                if u.email:
+                    user_info["email"] = u.email
+
+                # If the user has a full name, use that for the user name.
+                # However, a given user_name overrides the raw user.username,
+                # so only use that if this comment has no associated name.
+                if u.get_full_name():
+                    user_info["name"] = self.user.get_full_name()
+                elif not self.user_name:
+                    user_info["name"] = u.get_username()
+            self._user_info = user_info
+        return self._user_info
+    user_info = property(_get_user_info)
+
+    def _get_name(self):
+        return self.userinfo["name"]
+
+    def _set_name(self, val):
+        if self.user_id:
+            raise AttributeError(_("Name of the registered user can't be "
+                                   "changed"))
+        self.user_name = val
+    name = property(_get_name, _set_name, doc="Name of the author")
+
+    def _get_email(self):
+        return self.userinfo["email"]
+
+    def _set_email(self, val):
+        if self.user_id:
+            raise AttributeError(_("E-mail of registered user can't be "
+                                   "changed"))
+        self.user_email = val
+    email = property(_get_email, _set_email, doc="Email of the author")
+
+    class Meta:
+        ordering = ('submit_date',)
+        permissions = [("can_moderate", "Can moderate comments")]
+        verbose_name = _('Comment')
+        verbose_name_plural = _('Comments')
