4a4ee00ef79003ed4d98a3daca9e26cffc91210e
Visgean Skeloru <visgean@gmail.com>
2015-04-22 03:39:26 +0200

    Comment view with saving and lists

--- Seeder/class_based_comments/views.py
+++ Seeder/class_based_comments/views.py
@@ -1,9 +1,12 @@
 import forms
 
+from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import CreateView
 
+from models import Comment
 
-class CommentView(CreateView):
+
+class CommentView(CreateView, SingleObjectMixin):
     """
     View for creating and listing comments.
     """
@@ -12,18 +15,22 @@ class CommentView(CreateView):
     anonymous_threaded_comment_form = forms.AnonymousThreadedCommentForm
     registered_threaded_comment_form = forms.RegisteredThreadedCommentForm
 
-    prefix = 'comments'
+    prefix = 'comment'
     threaded = False
+
     # disable comments for anonymous users
     enforce_login = False
 
+    def get_context_data(self, **kwargs):
+        context = super(CommentView, self).get_context_data(**kwargs)
+        context['comments'] = Comment.objects.for_model(self.get_object())
+        return context
+
     def get_form(self, form_class=None):
         authenticated = self.request.user.is_authenticated()
-
         if self.enforce_login and not authenticated:
             raise NotImplemented('Report a bug to show interest in this '
-                                 'feature... :P')
-
+                                 'feature...')
         if self.threaded:
             if authenticated:
                 return self.registered_threaded_comment_form
@@ -32,3 +39,10 @@ class CommentView(CreateView):
             if authenticated:
                 return self.registered_comment_form
             return self.anonymous_comment_form
+
+    def form_valid(self, form):
+        user = self.request.user
+        comment = form.save(commit=False)
+        if user.is_authenticated():
+            comment.user = user
+        comment.save()
