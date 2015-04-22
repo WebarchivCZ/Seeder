afda4236ddf4d6dfaf6757d2045d5947a6710224
Visgean Skeloru <visgean@gmail.com>
2015-04-22 13:27:59 +0200

    working CommentView
    added http post processing

--- Seeder/class_based_comments/views.py
+++ Seeder/class_based_comments/views.py
@@ -1,12 +1,13 @@
 import forms
 
-from django.views.generic.base import View
+from django.http.response import HttpResponseRedirect
+from django.views.generic.base import View, TemplateResponseMixin
 from django.views.generic.detail import SingleObjectMixin
 
 from models import Comment
 
 
-class CommentView(SingleObjectMixin, View):
+class CommentView(TemplateResponseMixin, SingleObjectMixin, View):
     """
     View for creating and listing comments.
     """
@@ -19,20 +20,40 @@ class CommentView(SingleObjectMixin, View):
     enforce_login = False
     # enable threading of comments
     threaded = False
-
     form_name = 'comment_form'
 
+    def initialize_form(self, data=None):
+        """
+        This might be overridden if default does not accept target_object
+        argument
+        """
+        return self.comment_form(target_object=self.get_object(), data=data)
+
+    def post(self, request, *args, **kwargs):
+
+        form = self.initialize_form(data=request.POST)
+        user = self.request.user
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            if user.is_authenticated():
+                comment.user = user
+            comment.save()
+            return HttpResponseRedirect('')  # reload the page
+        else:
+            context = self.get_context_data(**kwargs)
+            context[self.form_name] = form
+            return self.render_to_response(context)
 
 
     def get_context_data(self, **kwargs):
         context = super(CommentView, self).get_context_data(**kwargs)
-        obj = self.get_object()
-        form_class = self.get_form()
         context['comments'] = Comment.objects.for_model(self.get_object())
-        context[self.form_name] = form_class(target_object=obj)
+        context[self.form_name] = self.initialize_form()
         return context
 
-    def get_form(self, form_class=None):
+    @property
+    def comment_form(self):
         authenticated = self.request.user.is_authenticated()
         if self.enforce_login and not authenticated:
             raise NotImplemented('Report a bug to show interest in this '
@@ -46,9 +67,26 @@ class CommentView(SingleObjectMixin, View):
                 return self.registered_comment_form
             return self.anonymous_comment_form
 
-    def form_valid(self, form):
-        user = self.request.user
-        comment = form.save(commit=False)
-        if user.is_authenticated():
-            comment.user = user
-        comment.save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
