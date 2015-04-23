d91e3dbab2c2c1e574f863020aa3d5202223fe30
Visgean Skeloru <visgean@gmail.com>
2015-04-23 15:22:05 +0200

    pep8 urls

--- Seeder/urls.py
+++ Seeder/urls.py
@@ -1,25 +1,26 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
-urlpatterns = patterns('',
+auth_patterns = patterns(
+    'django.contrib.auth.views',
+    url(r'^login/$', 'login', name='login'),
+    url(r'^logout/$', 'logout_then_login', name='logout'),
+    url(r'^passwd/$', 'password_change', name='passwd'),
+    url(r'^passwd/done/$', 'password_change_done', name='passwd_done'),
+    url(r'^reset/$', 'password_reset', name='passwd_reset'),
+    url(r'^reset/done/$', 'password_reset_done', name='passwd_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'password_reset_confirm', name='reset_confirm'),  # noqa
+    url(r'^reset/done/$', 'password_reset_complete', name='reset_complete'),
+)
+
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout_then_login, name='logout'),
-    # url(r'^password_change/$', auth_views.password_change, name='password_change'),
-    # url(r'^password_change/done/$', auth_views.password_change_done, name='password_change_done'),
-    # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    # url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     auth_views.password_reset_confirm, name='password_reset_confirm'),
-    # url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
-
-
-    url(r'^source/', include('source.urls')),
-    url(r'^publisher/', include('publishers.urls')),
-    url(r'^voting/', include('voting.urls')),
+    url(r'^auth/', include(auth_patterns, namespace='auth')),
+    url(r'^source/', include('source.urls', namespace='source')),
+    url(r'^publisher/', include('publishers.urls', namespace='publishers')),
+    url(r'^voting/', include('voting.urls', namespace='voting')),
 
     # beware: wild card regexp!
-    url(r'^', include('core.urls'))
+    url(r'^', include('core.urls', namespace='core'))
 )
