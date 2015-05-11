ef60f4d15f008a738539b1f172ff5e2e46d5c543
Visgean Skeloru <visgean@gmail.com>
2015-05-11 17:31:20 +0200

    more typos

--- Seeder/fabfile.py
+++ Seeder/fabfile.py
@@ -8,7 +8,7 @@ commands = {
         './manage.py syncdb --noinput',
         './manage.py migrate'],
     'static': [
-        './<manage.py bower_install>',
+        './manage.py bower_install',
         './manage.py collectstatic --noinput'],
     'pull': ['git pull --rebase']
 }
