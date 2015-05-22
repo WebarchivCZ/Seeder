316b75aa4d622374bcfbdc0f33deebd9d07737df
Visgean Skeloru <visgean@gmail.com>
2015-05-23 01:27:18 +0200

    password change, fixes #47

--- Seeder/core/views.py
+++ Seeder/core/views.py
@@ -1,9 +1,21 @@
-from django.views.generic.base import TemplateView
+from django.http.response import HttpResponseRedirect
+from django.views.generic.base import TemplateView, View
 from django.utils.translation import ugettext as _
-from utils import LoginMixin
+from django.contrib import messages
+
+from utils import LoginMixin, MessageView
 
 
 class DashboardView(LoginMixin, TemplateView):
     template_name = 'dashboard.html'
     title = _('Welcome')
     view_name = 'dashboard'
+
+
+class PasswordChangeDone(LoginMixin, MessageView, View):
+    """
+        Redirect page that adds success message
+    """
+    def get(self, request):
+        self.add_message(_('Password changed successfully.'), messages.SUCCESS)
+        return HttpResponseRedirect('/')
--- Seeder/urls.py
+++ Seeder/urls.py
@@ -1,22 +1,25 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from core.views import PasswordChangeDone
+
 auth_patterns = patterns(
     'django.contrib.auth.views',
     url(r'^login/$', 'login', name='login'),
     url(r'^logout/$', 'logout_then_login', name='logout'),
-    url(r'^passwd/$', 'password_change', name='passwd'),
-    url(r'^passwd/done/$', 'password_change_done', name='passwd_done'),
-    url(r'^reset/$', 'password_reset', name='passwd_reset'),
-    url(r'^reset/done/$', 'password_reset_done', name='passwd_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'password_reset_confirm', name='reset_confirm'),  # noqa
-    url(r'^reset/done/$', 'password_reset_complete', name='reset_complete'),
+    url(r'^passwd/$', 'password_change', name='password_change'),
+    url(r'^reset/$', 'password_reset', name='password_reset'),
+    url(r'^reset/done/$', 'password_reset_done', name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'password_reset_confirm', name='password_reset_confirm'),  # noqa
+    url(r'^reset/done/$', 'password_reset_complete', name='password_reset_complete'),  # noqa
+    url(r'^passwd/done/$', PasswordChangeDone.as_view(), name='password_change_done'),  # noqa
+
 )
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^auth/', include(auth_patterns, namespace='auth')),
+    url(r'^auth/', include(auth_patterns)),
     url(r'^source/', include('source.urls', namespace='source')),
     url(r'^publisher/', include('publishers.urls', namespace='publishers')),
     url(r'^voting/', include('voting.urls', namespace='voting')),
