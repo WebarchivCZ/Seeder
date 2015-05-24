543d5791e2080f5d73b3af790e27be1252f373eb
Visgean Skeloru <visgean@gmail.com>
2015-05-24 03:37:54 +0200

    MultipleFormView was not used
    form wizard proved to be more useful

--- Seeder/core/generic_views.py
+++ Seeder/core/generic_views.py
@@ -6,7 +6,6 @@ import reversion
 
 from django.contrib import messages
 from django.http.response import HttpResponseRedirect
-from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic.base import View
@@ -15,42 +14,13 @@ from django.views.generic import DetailView
 from django.views.generic.edit import UpdateView
 
 from django_tables2 import SingleTableView
-from utils import dict_diff, merge_dicts
+from utils import dict_diff
 
 
 class LoginMixin(object):
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         return super(LoginMixin, self).dispatch(request, *args, **kwargs)
-
-
-class MultipleFormView(TemplateView):
-    """
-    View mixin that handles multiple forms / formsets.
-    After the successful data is inserted ``self.forms_valid`` is called.
-    """
-    form_classes = {}
-
-    def initialize_forms(self, data=None):
-        return {name: form(prefix=name, data=data)
-                for name, form in self.form_classes.items()}
-
-    def get_context_data(self, **kwargs):
-        context = super(MultipleFormView, self).get_context_data(**kwargs)
-        return merge_dicts(context, self.initialize_forms())
-
-    def post(self, request):
-        forms_initialized = self.initialize_forms(data=request.POST)
-        valid = all([form.is_valid() for form in forms_initialized.values()])
-
-        if valid:
-            return self.forms_valid(forms_initialized)
-        else:
-            context = merge_dicts(self.get_context_data(), forms_initialized)
-            return self.render_to_response(context)
-
-    def forms_valid(self, form_instances):
-        raise NotImplementedError('Implement this in your view!')
 
 
 class MessageView(object):
