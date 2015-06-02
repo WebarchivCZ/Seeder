db97a957726872623ccae1af4ae3c5624cc460c0
Visgean Skeloru <visgean@gmail.com>
2015-06-02 23:01:40 +0200

    contract urls

---
+++ Seeder/contracts/urls.py
@@ -0,0 +1,10 @@
+import views
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.ListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/detail$', views.Detail.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/edit$', views.Edit.as_view(), name='edit'),
+    url(r'^(?P<pk>\d+)/history$', views.History.as_view(), name='history'),
+)
