4aa81f7ac90c8d9952e1f54bd86b169d412d7fb9
Visgean Skeloru <visgean@gmail.com>
2015-06-03 20:44:18 +0200

    process_voting_round: create new contract

--- Seeder/voting/signals.py
+++ Seeder/voting/signals.py
@@ -1,5 +1,5 @@
 # pylint: disable=W0613
-
+from datetime import datetime
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -7,6 +7,8 @@ from voting import constants
 from source.models import Source
 from voting.models import VotingRound
 from source import constants as source_constants
+from contracts.models import Contract
+from contracts import constants as contract_constants
 
 
 @receiver(signal=post_save, sender=Source)
@@ -28,9 +30,15 @@ def process_voting_round(instance, created, **kwargs):
     """
     if not created:
         source = instance.source
-        if (instance.state == constants.VOTE_APPROVE and
-                source.contract_set.valid()):
-            source.state = source_constants.STATE_RUNNING
+        if instance.state == constants.VOTE_APPROVE:
+            if source.contract_set.valid():
+                source.state = source_constants.STATE_RUNNING
+            else:
+                contract = Contract(
+                    source=source,
+                    date_start=datetime.now(),
+                    contract_type=contract_constants.CONTRACT_PROPRIETARY)
+                contract.save()
         else:
             source.state = constants.VOTE_TO_SOURCE[instance.state]
         source.save()
