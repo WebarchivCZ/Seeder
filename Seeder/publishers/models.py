166103baaa74f130c95cadb2a69f19ec44588d3d
Visgean Skeloru <visgean@gmail.com>
2015-06-06 17:30:55 +0200

    Publishers.ContactPerson
    Bigger publishers with a lot of source might have different people handling different sources.

--- Seeder/publishers/models.py
+++ Seeder/publishers/models.py
@@ -13,8 +13,6 @@ class Publisher(BaseModel):
         Publisher of the Source(s), Publisher can have multiple contacts.
     """
     name = models.CharField(_('Name'), max_length=64)
-    email = models.EmailField(_('E-mail'), blank=True, null=True)
-    phone = models.CharField(_('Phone'), blank=True, null=True, max_length=64)
     website = models.URLField(_('Website'), blank=True, null=True)
 
     class Meta:
@@ -26,3 +24,26 @@ class Publisher(BaseModel):
 
     def get_absolute_url(self):
         return reverse('publishers:detail', kwargs={'pk': self.id})
+
+
+@reversion.register(exclude=('last_changed',))
+class ContactPerson(BaseModel):
+    """
+        Bigger publishers with a lot of source might have different people
+        handling different sources.
+    """
+    publisher = models.ForeignKey(Publisher)
+
+    name = models.CharField(_('Name'), max_length=64)
+    email = models.EmailField(_('E-mail'), blank=True, null=True)
+    phone = models.CharField(_('Phone'), blank=True, null=True, max_length=64)
+
+    class Meta:
+        verbose_name = _('Publisher contact')
+        verbose_name_plural = _('Publisher contacts')
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('publishers:detail', kwargs={'pk': self.publisher.id})
