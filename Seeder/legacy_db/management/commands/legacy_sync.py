4fa929b4e5af87a74664b9a1640f52be3f7fa53f
Visgean Skeloru <visgean@gmail.com>
2015-06-09 01:33:37 +0200

    legacy sync management command

---
+++ Seeder/legacy_db/management/commands/legacy_sync.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from legacy_db.conversion import CONVERSIONS
+
+
+class Command(BaseCommand):
+    help = 'Synchronizes legacy database with the new db'
+
+    def handle(self, *args, **options):
+        for conversion in CONVERSIONS:
+            conversion().start_conversion()
