49e83db14f98764cc1742275cd70271efd0ad00f
Visgean Skeloru <visgean@gmail.com>
2015-06-09 01:36:54 +0200

    curators in models.py  => processed

--- Seeder/legacy_db/models.py
+++ Seeder/legacy_db/models.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class TransferRecord(models.Model):
-    original_type = models.ForeignKey(ContentType)
+    original_type = models.ForeignKey(ContentType, related_name='transfer')
     original_id = models.PositiveIntegerField()
     original_object = GenericForeignKey('original_type', 'original_id')
 
@@ -14,3 +14,27 @@ class TransferRecord(models.Model):
 
     last_update = models.DateTimeField(auto_created=True, auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+
+class Curators(models.Model):
+    username = models.CharField(unique=True, max_length=50)
+    password = models.CharField(max_length=50)
+    vocative = models.CharField(max_length=30)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    icq = models.IntegerField(blank=True, null=True)
+    skype = models.CharField(max_length=100, blank=True, null=True)
+    active = models.IntegerField()
+    active_from = models.DateField(blank=True, null=True)
+    active_to = models.DateField(blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
+    logins = models.IntegerField()
+    last_login = models.IntegerField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.username
+
+    class Meta:
+        managed = False
+        db_table = 'curators'
