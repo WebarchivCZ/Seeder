083f5a2e77700c53c779e3abe9911678d3478388
Visgean Skeloru <visgean@gmail.com>
2015-06-12 15:45:23 +0200

    contract 0002 migration for last commit

---
+++ Seeder/contracts/migrations/0002_auto_20150612_1343.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contracts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='contract',
+            name='access_token',
+            field=models.CharField(max_length=37, null=True, blank=True),
+        ),
+    ]
