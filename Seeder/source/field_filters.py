e7c6cb8377592ba46d04706e1668ea4431b8de77
Visgean Skeloru <visgean@gmail.com>
2015-06-18 02:41:04 +0200

    Faster source list
    no need to list all the publishers

--- Seeder/source/field_filters.py
+++ Seeder/source/field_filters.py
@@ -1,9 +1,12 @@
 import models
+import django_filters
 
 from core.utils import EmptyFilter
 
 
 class SourceFilter(EmptyFilter):
+    publisher = django_filters.CharFilter(lookup_type='name__icontains')
+
     class Meta:
         model = models.Source
         fields = ('name', 'owner', 'publisher', 'state',
