e6a4820669fde0f9fc7233e964bb0536d0c700ca
Visgean Skeloru <visgean@gmail.com>
2015-08-02 23:41:19 +0200

    remove source from ContractTable

--- Seeder/contracts/tables.py
+++ Seeder/contracts/tables.py
@@ -6,15 +6,14 @@ from core.utils import AbsoluteURLColumn, NaturalDatetimeColumn
 
 class ContractTable(tables.Table):
     link = AbsoluteURLColumn(accessor='__unicode__')
-    source = AbsoluteURLColumn()
     publisher = AbsoluteURLColumn(accessor='source.publisher')
     created = NaturalDatetimeColumn()
     last_changed = NaturalDatetimeColumn()
 
     class Meta:
         model = models.Contract
-        fields = ('link', 'source', 'publisher', 'state', 'valid_to',
-                  'valid_from', 'contract_type', 'in_communication', 'created',
+        fields = ('link', 'publisher', 'state', 'valid_to',
+                  'valid_from', 'open_source', 'in_communication', 'created',
                   'last_changed')
 
         attrs = {
