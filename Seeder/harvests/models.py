7613f441dfe397a3fd4ed334f8e693a33d411607
Visgean Skeloru <visgean@gmail.com>
2015-12-10 02:01:21 +0100

    Different harvests models
    now harvests contain all the details and seeds, no need for seed lists..

--- Seeder/harvests/models.py
+++ Seeder/harvests/models.py
@@ -1,25 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 from core.models import BaseModel
 from source.constants import SOURCE_FREQUENCY_PER_YEAR
-from source_lists.models import HarvestList
+from source.models import Source
 
 
 class Harvest(BaseModel):
     """
         Represents the event of harvesting the sources
     """
-    responsible_curator = models.ForeignKey(User)
+    TYPE_REGULAR = 1
+    TYPE_THEMED = 2
 
-    target_frequency = models.CharField(
+    HARVEST_TYPES = (
+        (TYPE_REGULAR, _('Regular harvest')),
+        (TYPE_THEMED, _('Seasonal harvest')),
+    )
+
+    responsible_curator = models.ForeignKey(User)
+    harvest_type = models.SmallIntegerField(
+        verbose_name=('Harvest type'),
+        choices=HARVEST_TYPES
+    )
+
+    target_frequency = models.IntegerField(
+        verbose_name=_('Frequency'),
         choices=SOURCE_FREQUENCY_PER_YEAR,
         blank=True,
         null=True
     )
 
-    target_list = models.OneToOneField(
-        HarvestList,
-        null=True,
-        blank=True
+    custom_sources = models.ManyToManyField(
+        Source,
+        blank=True,
+        null=True
+    )
+
+    custom_seeds = models.TextField(
+        verbose_name=_('Custom seeds URL'),
+        help_text=_('One URL per line'),
+        blank=True,
+        null=True
     )
