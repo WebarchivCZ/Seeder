bb41affef8a5b13eb653f56e022f50b80be259b8
Visgean Skeloru <visgean@gmail.com>
2015-12-13 16:15:36 +0100

    translations for QA model

--- Seeder/qa/models.py
+++ Seeder/qa/models.py
@@ -1,13 +1,26 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from core.models import BaseModel
 from source.models import Source
+from source import constants as source_constants
 
 
 class QualityAssuranceCheck(BaseModel):
-    source = models.ForeignKey(Source)
+    source = models.ForeignKey(
+        verbose_name=_('Source'),
+        to=Source)
 
-    checked_by = models.ForeignKey(User)
-    result_score = models.PositiveSmallIntegerField()
-    comment = models.TextField(blank=True)
+    checked_by = models.ForeignKey(
+        verbose_name=_('Checked by'),
+        to=User)
+
+    comment = models.TextField(
+        verbose_name=_(''),
+        blank=True)
+
+    source_action = models.CharField(
+        verbose_name=_('State'),
+        max_length=15,
+        choices=source_constants.SOURCE_STATES)
