9b393b792354094adeb04d381b7245111f7bbad3
Visgean Skeloru <visgean@gmail.com>
2015-12-13 15:28:32 +0100

    QA model with link to Source

--- Seeder/qa/models.py
+++ Seeder/qa/models.py
@@ -2,11 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from core.models import BaseModel
+from source.models import Source
 
 
 class QualityAssuranceCheck(BaseModel):
+    source = models.ForeignKey(Source)
+
     checked_by = models.ForeignKey(User)
     result_score = models.PositiveSmallIntegerField()
     comment = models.TextField(blank=True)
-
-    
