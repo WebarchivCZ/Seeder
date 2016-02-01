94eec02aab54225cd50d4b70c9bba5a7b72c26a5
Visgean Skeloru <visgean@gmail.com>
2016-02-01 18:14:22 +0100

    remove obsolete harvest views

--- Seeder/harvests/views.py
+++ Seeder/harvests/views.py
@@ -24,38 +24,10 @@ class HarvestView(generic_views.LoginMixin):
 
 class CalendarView(HarvestView, URLView, TemplateView):
     template_name = 'calendar.html'
-    url = U / 'calendar'
+    url = U
     url_name = 'calendar'
 
-
-class ScheduleView(HarvestView, URLView, FormView):
-    url = U / 'schedule'
-    url_name = 'schedule'
-    template_name = 'formset_verbose.html'
-
-    def get_form_class(self):
-        return formset_factory(
-            form=forms.HarvestForm,
-            can_delete=True,
-            extra=0)
-
-    def get_initial(self):
-        return get_initial_scheduled_data(date.today() + INITIAL_OFFSET)
-
-    def form_valid(self, formset):
-        for form in formset:
-            harvest = form.save(commit=False)
-            harvest.harvest_type = models.Harvest.TYPE_REGULAR
-            harvest.save()
-        return redirect('harvests:list')
-
-
-class ListView(HarvestView, URLView, generic_views.FilteredListView):
-    url = U
-    url_name = 'list'
-
-    add_link = 'harvests:schedule'
-    add_link_title = _('Schedule')
-
-    table_class = tables.HarvestTable
-    filter_class = field_filters.HarvestFilter
+    def get_context_data(self, **kwargs):
+        context = super(CalendarView, self).get_context_data(**kwargs)
+        context['harvest_form'] = forms.HarvestCreateForm()
+        return context
