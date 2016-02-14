8b7c6a9a5cbba213b84fe4484c9708213dffebd2
Visgean Skeloru <visgean@gmail.com>
2016-02-14 23:19:28 +0100

    travis config and dummy settings for tests

---
+++ Seeder/settings/tests.py
@@ -0,0 +1,22 @@
+from base import *
+
+SECRET_KEY = 'test'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'sqlite3.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
