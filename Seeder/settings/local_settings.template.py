a8b2f43e140d84225290fe3a98548e286e268b60
Visgean Skeloru <visgean@gmail.com>
2016-04-03 18:59:12 +0200

    settings template with docker variables

--- Seeder/settings/local_settings.template.py
+++ Seeder/settings/local_settings.template.py
@@ -1,42 +1,43 @@
 from base import *
 
-
-SECRET_KEY = ''
-assert SECRET_KEY, 'You must set secret key!'
+SECRET_KEY = 'sdfvsdfvsdfvsdfv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['example.com', 'www.example.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 # since we use special PostgresSQL fields we cannot simply use any db.
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '<db name>',
-        'USER': '<db user>',
-        'PASSWORD': '<db pass>'
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
     },
     'legacy_seeder': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '???',
-        'USER': '???',
-        'PASSWORD': '???'
+        'NAME': 'legacy_db',
+        'USER': 'legacy',
+        'PASSWORD': 'legacy',
+        'HOST': 'mysql',
     }
 }
+
 
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://haystack:9200/',
+        'URL': 'http://elastic:9200/',
         'INDEX_NAME': 'haystack',
     },
 }
 
 
-RAVEN_CONFIG = {
-    'dsn': 'https://<key>:<secret>@app.getsentry.com/<project>',
-}
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LEGACY_DB_CONNECTED = True
