82025d9aac239534c5399e6b730e4238beefa791
Visgean Skeloru <visgean@gmail.com>
2016-04-03 20:33:25 +0200

    preparing custom search view

--- Seeder/core/views.py
+++ Seeder/core/views.py
@@ -8,6 +8,7 @@ from django.views.generic.edit import UpdateView
 from django.utils.http import is_safe_url
 from django.utils import translation
 
+from haystack.generic_views import SearchView
 from urljects import U, URLView
 
 from generic_views import LoginMixin, MessageView
@@ -92,3 +93,12 @@ class PasswordChangeDone(MessageView, View):
     def get(self, request):
         self.add_message(_('Password changed successfully.'), messages.SUCCESS)
         return HttpResponseRedirect('/')
+
+
+class SimpleSearchView(SearchView, URLView):
+    """
+    Simpler search view that modified form class so that it has only one query
+    field without selecting models
+    """
+    url = U / 'search'
+    url_name = 'simple_haystack_search'
