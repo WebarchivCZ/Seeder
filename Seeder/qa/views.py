c7b7c05fea19a325a5356e2b993c5330fe97f0c9
Visgean Skeloru <visgean@gmail.com>
2016-04-19 20:26:16 +0200

    QA forms with link to seeds
    fixes #220

--- Seeder/qa/views.py
+++ Seeder/qa/views.py
@@ -20,10 +20,12 @@ class QAView(LoginMixin):
 class QACreate(QAView, FormView, DetailView, URLView):
     model = Source
     form_class = forms.QACreateForm
-    template_name = 'edit_form.html'
+    template_name = 'qa_form.html'
+    context_object_name = 'source'
 
     url = U / 'source' / pk / 'create'
     url_name = 'create'
+
 
     def form_valid(self, form):
         qa = form.save(commit=False)
@@ -36,7 +38,7 @@ class QACreate(QAView, FormView, DetailView, URLView):
 
 class QAEdit(QAView, EditView, URLView):
     form_class = forms.QAEditForm
-    template_name = 'edit_form.html'
+    template_name = 'qa_form.html'
 
     url = U / pk / 'edit'
     url_name = 'edit'
@@ -45,6 +47,11 @@ class QAEdit(QAView, EditView, URLView):
         redirect = super().form_valid(form)
         self.get_object().process_action()
         return redirect
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['source'] = self.get_object().source
+        return context
 
 
 class QADetail(QAView, DetailView, CommentViewGeneric, URLView):
