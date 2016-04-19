10d5f882fcce8ff685c35444555cdd6d08bc5d13
Visgean Skeloru <visgean@gmail.com>
2016-04-19 17:01:27 +0200

    QA table
    fixes #211

---
+++ Seeder/qa/tables.py
@@ -0,0 +1,24 @@
+import models
+import django_tables2 as tables
+
+from core.utils import AbsoluteURLColumn, NaturalDatetimeColumn
+
+
+class QATable(tables.Table):
+    pk = AbsoluteURLColumn(accessor='id')
+    created = NaturalDatetimeColumn()
+    last_changed = NaturalDatetimeColumn()
+
+    class Meta:
+        model = models.QualityAssuranceCheck
+        fields = [
+            'pk',
+            'source',
+            'content_changed',
+            'technical_quality_changed',
+            'source_action',
+        ]
+
+        attrs = {
+            'class': 'table table-striped table-hover'
+        }
