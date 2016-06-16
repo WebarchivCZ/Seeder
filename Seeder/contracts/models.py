50146182fe70f20dcab4b7ce27ba45ba334584dd
Visgean Skeloru <me@visgean.me>
2016-06-16 23:35:45 +0200

    desc order for contracts fixes #264

--- Seeder/contracts/models.py
+++ Seeder/contracts/models.py
@@ -113,7 +113,7 @@ class Contract(BaseModel):
 
     class Meta:
         unique_together = ('contract_number', 'year')
-        ordering = ['created']
+        ordering = ['-created']
 
     def __str__(self):
         if self.state == constants.CONTRACT_STATE_NEGOTIATION:
