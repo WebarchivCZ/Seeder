7aaae929736ed360350b001f79a6761afbf3a6df
Visgean Skeloru <me@visgean.me>
2016-07-20 12:33:50 +0200

    fix buggy contract time filters

--- Seeder/contracts/field_filters.py
+++ Seeder/contracts/field_filters.py
@@ -1,10 +1,13 @@
 from . import models
 
-from core.custom_filters import EmptyFilter
+from core.custom_filters import EmptyFilter, DateRangeFilter
 
 
 class ContractFilter(EmptyFilter):
+    valid_to = DateRangeFilter()
+    valid_from = DateRangeFilter()
+
     class Meta:
         model = models.Contract
-        fields = ('state', 'valid_to', 'valid_from', 'creative_commons',
+        fields = ('state', 'valid_from', 'valid_to', 'creative_commons',
                   'in_communication')
