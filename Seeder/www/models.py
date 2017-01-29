b72cb4ea550394bb24ece66d425335c2cd0491f9
Visgean Skeloru <me@visgean.me>
2017-01-29 02:06:13 +0000

    add topic collections models

--- Seeder/www/models.py
+++ Seeder/www/models.py
@@ -7,14 +7,23 @@ from source.models import Source
 
 class NewsObject(BaseModel):
     title = models.CharField(max_length=150)
-	text = models.TextField()
-	image = models.ImageField(upload_to='photos')    
+    annotation = models.TextField()
+    image = models.ImageField(upload_to='photos')    
 
     source_1 = models.ForeignKey(Source, on_delete=models.DO_NOTHING, null=True, blank=True)
     source_2 = models.ForeignKey(Source, on_delete=models.DO_NOTHING, null=True, blank=True)
 
 
 class SearchLog(models.Model):
-	search_term = models.CharField(max_length=256)
-	log_time = models.DateTimeField(default=timezone.now, editable=False)
-	ip_address = models.GenericIPAddressField()
+    search_term = models.CharField(max_length=256)
+    log_time = models.DateTimeField(default=timezone.now, editable=False)
+    ip_address = models.GenericIPAddressField()
+
+
+class TopicCollection(BaseModel):
+    title = models.CharField(max_length=150)
+    annotation = models.TextField()
+    image = models.ImageField(upload_to='photos')    
+
+    sources = models.ManyToManyField(Source)
+    
