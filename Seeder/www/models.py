0f1dd70845c13cafb8ddcff6f501e9404c7e1a01
Visgean Skeloru <me@visgean.me>
2017-01-30 01:56:34 +0000

    RichTextField for public models and image is optional
    fixes #321

--- Seeder/www/models.py
+++ Seeder/www/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+from ckeditor.fields import RichTextField
+
 from core.models import BaseModel, DatePickerField
 from source.models import Source
 
 
 class NewsObject(BaseModel):
     title = models.CharField(max_length=150)
-    annotation = models.TextField()
-    image = models.ImageField(upload_to='photos')    
+    annotation = RichTextField()
+    image = models.ImageField(upload_to='photos', null=True, blank=True)    
 
     source_1 = models.ForeignKey(
         Source, 
@@ -21,6 +23,9 @@ class NewsObject(BaseModel):
         on_delete=models.DO_NOTHING, 
         null=True, blank=True,
         related_name='news_b')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = _('News article')
@@ -35,7 +40,15 @@ class SearchLog(models.Model):
 
 class TopicCollection(BaseModel):
     title = models.CharField(max_length=150)
-    annotation = models.TextField()
+    annotation = RichTextField()
     image = models.ImageField(upload_to='photos')    
 
     sources = models.ManyToManyField(Source)
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Topic collection')
+        verbose_name_plural = _('Topic collections')
