dcf03dac3f409d3fd43929727ef15b72e1cde534
Visgean Skeloru <me@visgean.me>
2017-01-31 00:03:03 +0000

    fix language switching
    fixes #333

--- Seeder/www/views_non_localized.py
+++ Seeder/www/views_non_localized.py
@@ -1,7 +1,7 @@
 from django.views.generic.base import View
 from django.http.response import HttpResponseRedirect
 from django.utils.http import is_safe_url
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse
 from django.utils import translation
 
 from urljects import U, URLView
@@ -13,4 +13,5 @@ class ChangeLanguage(View, URLView):
     def get(self, request, code):
         if translation.check_for_language(code):
             request.session[translation.LANGUAGE_SESSION_KEY] = code
-        return HttpResponseRedirect(reverse_lazy('www:index'))
+            translation.activate(code)
+        return HttpResponseRedirect(reverse('www:index'))
