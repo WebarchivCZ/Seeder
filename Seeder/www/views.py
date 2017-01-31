c7a0d09e03fd5046b83be23c48dde86289b110c9
Visgean Skeloru <me@visgean.me>
2017-01-31 02:14:14 +0000

    add rest of the about section

--- Seeder/www/views.py
+++ Seeder/www/views.py
@@ -30,6 +30,7 @@ class Index(TemplateView, URLView):
 class About(TemplateView, URLView):
     template_name = 'about/about.html'
     view_name = 'about'
+    sub_view_name = 'about'
 
     url = U / _('about_url')
     url_name = 'about'
@@ -47,3 +48,47 @@ class MoreAbout(TemplateView, URLView):
         context['footerFullBorder'] = True
         return context
 
+
+class AboutHarvest(TemplateView, URLView):
+    template_name = 'about/harvests.html'
+    view_name = 'about'
+    sub_view_name = 'harvests'
+
+    url = U / _('about_harvests_url')
+    url_name = 'about_harvests'
+
+
+class AboutTerminology(TemplateView, URLView):
+    template_name = 'about/terminology.html'
+    view_name = 'about'
+    sub_view_name = 'terminology'
+
+    url = U / _('about_terminology_url')
+    url_name = 'about_terminology'
+
+
+class AboutDocuments(TemplateView, URLView):
+    template_name = 'about/documents.html'
+    view_name = 'about'
+    sub_view_name = 'documents'
+
+    url = U / _('about_documents_url')
+    url_name = 'about_documents'
+
+
+class AboutGraphics(TemplateView, URLView):
+    template_name = 'about/graphics.html'
+    view_name = 'about'
+    sub_view_name = 'graphics'
+
+    url = U / _('about_graphics_url')
+    url_name = 'about_graphics'
+
+
+class AboutContact(TemplateView, URLView):
+    template_name = 'about/contact.html'
+    view_name = 'about'
+    sub_view_name = 'contact'
+
+    url = U / _('about_contact_url')
+    url_name = 'about_contact'
