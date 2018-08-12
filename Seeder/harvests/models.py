e3db91f90ee3981178fb105630b89aeec6f6d115
Visgean Skeloru <me@visgean.me>
2018-08-12 19:09:22 +0100

    add link for date url to harvest

--- Seeder/harvests/models.py
+++ Seeder/harvests/models.py
@@ -169,9 +169,14 @@ class Harvest(HarvestAbstractModel):
             base_set.update(collection.get_seeds())
         return base_set
 
-
     def get_absolute_url(self):
         return reverse('harvests:detail', args=[str(self.id)])
+
+    def get_date_url(self):
+        return reverse('harvests:urls_by_time', args=[
+            str(self.scheduled_on.isoformat()),
+            str(self.target_frequency[0]),
+        ])
 
     def freeze_seeds(self):
         """
