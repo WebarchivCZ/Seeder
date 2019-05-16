9d8598c7f98539b2214433916503fc57725df112
Fasand <peter@petermanas.com>
2019-05-16 17:03:40 +0100

    update contracts filter to modern syntax + add publisher name

--- Seeder/contracts/field_filters.py
+++ Seeder/contracts/field_filters.py
@@ -4,10 +4,16 @@ from core.custom_filters import EmptyFilter, DateRangeFilter
 
 
 class ContractFilter(EmptyFilter):
-    valid_to = DateRangeFilter()
     valid_from = DateRangeFilter()
+    valid_to = DateRangeFilter()
 
     class Meta:
         model = models.Contract
-        fields = ('state', 'valid_from', 'valid_to', 'creative_commons',
-                  'in_communication')
+        fields = {
+            'publisher__name': ('icontains',),
+            'state': ('exact',),
+            'valid_from': ('exact',),
+            'valid_to': ('exact',),
+            'creative_commons': ('exact',),
+            'in_communication': ('exact',),
+        }
