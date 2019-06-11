c00475343ff3035300d20390cd0539d13f4727d8
westfood <westfood@gmail.com>
2019-06-11 13:35:04 +0200

    There are not requirements-dev.txt, removing it.

--- Seeder/prepare.sh
+++ Seeder/prepare.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 pip3 install -r ../requirements.txt --upgrade
-pip3 install -r ../requirements_dev.txt --upgrade
 python3 ./manage.py migrate
 
 python3 ./manage.py collectstatic --noinput
