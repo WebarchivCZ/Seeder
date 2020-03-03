c401452dbaa25864ccdc32efd9bf4ea18c6c03c5
westfood <westfood@gmail.com>
2020-03-03 10:43:11 +0100

    Always clear static_root during dpeloyment.
    Deployment will be slower, but it will fix issues with timestamp driven 
    updates of static_root.

--- Seeder/prepare.sh
+++ Seeder/prepare.sh
@@ -3,6 +3,6 @@
 pip3 install -r ../requirements.txt --upgrade
 python3 ./manage.py migrate
 
-python3 ./manage.py collectstatic --noinput
+python3 ./manage.py collectstatic --noinput --clear
 
 python3 ./manage.py crontab add
