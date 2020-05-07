6805f551ac83a2267c46e9b0bc17eb6ed91bc94d
Fasand <peter@petermanas.com>
2020-05-07 15:09:10 +0200

    add match_cc_types to prepare.sh so it runs on test server

--- Seeder/prepare.sh
+++ Seeder/prepare.sh
@@ -2,5 +2,7 @@
 
 pip3 install -r ../requirements.txt --upgrade
 python3 ./manage.py migrate
+# Make sure it runs at least once but every time also works
+python3 ./manage.py match_cc_types
 
 python3 ./manage.py crontab add
