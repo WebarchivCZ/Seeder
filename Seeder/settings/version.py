69b1114ad3f8297dff49f88bb67a7d0409c7c15d
Fasand <peter@petermanas.com>
2020-06-03 11:38:49 +0200

    add basic error handling to version.py

--- Seeder/settings/version.py
+++ Seeder/settings/version.py
@@ -1,16 +1,20 @@
 import subprocess
 from subprocess import PIPE
 
-# git rev-parse --abbrev-ref HEAD
-BRANCH = subprocess.run(
-    ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
-    stdout=PIPE, stderr=PIPE).stdout.decode('utf-8').strip()
-# git rev-parse --short HEAD
-COMMIT_ID = subprocess.run(
-    ['git', 'rev-parse', '--short', 'HEAD'],
-    stdout=PIPE, stderr=PIPE).stdout.decode('utf-8').strip()
-# git rev-list --count HEAD
-NUM_COMMITS = int(subprocess.run(
-    ['git', 'rev-list', '--count', 'HEAD'], stdout=PIPE, stderr=PIPE).stdout)
+try:
+    # git rev-parse --abbrev-ref HEAD
+    BRANCH = subprocess.run(
+        ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+        stdout=PIPE, stderr=PIPE).stdout.decode('utf-8').strip()
+    # git rev-parse --short HEAD
+    COMMIT_ID = subprocess.run(
+        ['git', 'rev-parse', '--short', 'HEAD'],
+        stdout=PIPE, stderr=PIPE).stdout.decode('utf-8').strip()
+    # git rev-list --count HEAD
+    NUM_COMMITS = int(subprocess.run(
+        ['git', 'rev-list', '--count', 'HEAD'],
+        stdout=PIPE, stderr=PIPE).stdout)
 
-VERSION = f"{BRANCH}@{COMMIT_ID}@{NUM_COMMITS}"
+    VERSION = f"{BRANCH}@{COMMIT_ID}@{NUM_COMMITS}"
+except:
+    VERSION = "Version cannot be loaded"
