325c71c6bd14f9deaf4780853220196403d7cc7b
Fasand <peter@petermanas.com>
2020-11-03 17:03:05 +0100

    improve urltags.py: allow url parameter delete by setting to None

--- Seeder/www/templatetags/urltags.py
+++ Seeder/www/templatetags/urltags.py
@@ -3,8 +3,12 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def url_replace(context, **kwargs):
     query = context['request'].GET.dict()
     query.update(kwargs)
+    # Delete keys which are set to None
+    for delete in [k for k, v in query.items() if v is None]:
+        query.pop(delete)
     return urlencode(query)
