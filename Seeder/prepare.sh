59d4204af7ef8153d75647a2eb597079a4b3b72d
Fasand <peter@petermanas.com>
2021-05-17 09:35:15 +0200

    add collectstatic to prepare.sh

--- Seeder/prepare.sh
+++ Seeder/prepare.sh
@@ -4,5 +4,5 @@ pip3 install -r ../requirements.txt --upgrade
 python3 ./manage.py migrate
 # Make sure it runs at least once but every time also works
 python3 ./manage.py match_cc_types
-
+python3 ./manage.py collectstatic --noinput --clear
 python3 ./manage.py crontab add
