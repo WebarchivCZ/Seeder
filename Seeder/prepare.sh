b90d37a88b6d36a3ed2fc034899a44f504120794
Fasand <peter@petermanas.com>
2023-12-10 14:28:06 +0100

    fix CRON not starting, update default manage.py DJANGO_SETTINGS_MODULE, log to /var/log/cron.log

--- Seeder/prepare.sh
+++ Seeder/prepare.sh
@@ -6,3 +6,5 @@ python3 ./manage.py migrate
 python3 ./manage.py match_cc_types
 python3 ./manage.py collectstatic --noinput --clear
 python3 ./manage.py crontab add
+# Start cron because it doesn't start on its own or from Dockerfile
+/etc/init.d/cron start
