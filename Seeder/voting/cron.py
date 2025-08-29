1aaf5779786755cfca17d96338de0711e2416cb3
Fasand <peter@petermanas.com>
2025-08-29 12:23:30 +0200

    #700 update revive_postponed_rounds cron to filter for '<= today' instead of today exactly

--- Seeder/voting/cron.py
+++ Seeder/voting/cron.py
@@ -4,8 +4,15 @@ from . import constants
 
 
 def revive_postponed_rounds():
-    today = date.today()
-    for voting_round in VotingRound.objects.filter(postponed_until=today):
+    """
+    Revive VotingRounds with postponsed_until date set which has passed.
+    """
+    rounds = VotingRound.objects.filter(postponed_until__lte=date.today())
+    if rounds.count() > 0:
+        print(f"Reviving {rounds.count()} postponed rounds:")
+    for voting_round in rounds:
+        print(f"- {voting_round.source.name} (postponed until "
+              f"{voting_round.postponed_until:%d.%m.%Y})")
         voting_round.state = constants.VOTE_INITIAL
         voting_round.postponed_until = None
         voting_round.save()
